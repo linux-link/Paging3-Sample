92c9919f248ececf065d9cbaa1e92d84f22f94c7
wujia0916 <wujia0916@thundersoft.com>
2020-08-15 17:48:52 +0800

    submit network brach

--- app/src/main/java/com/wujia/jetpack/paging3/sample/common/ViewModelFactory.kt
+++ app/src/main/java/com/wujia/jetpack/paging3/sample/common/ViewModelFactory.kt
@@ -3,16 +3,16 @@ package com.wujia.jetpack.paging3.sample.common
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.wujia.jetpack.paging3.sample.data.NetworkRepository
-import com.wujia.jetpack.paging3.sample.ui.NetworkViewModel
+import com.wujia.jetpack.paging3.sample.ui.GithubViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class ViewModelFactory(private val repository: NetworkRepository) : ViewModelProvider.Factory {
 
     @ExperimentalCoroutinesApi
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(NetworkViewModel::class.java)) {
+        if (modelClass.isAssignableFrom(GithubViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return NetworkViewModel(
+            return GithubViewModel(
                 repository
             ) as T
         }
